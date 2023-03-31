8f8f5a9676ef04a9278030b6a38462c5ff7f6ac3
Dnl1016 <61329285+Dnl1016@users.noreply.github.com>
2023-03-30 20:55:16 -0500

    otro comit màs

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,17 +1,17 @@
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-async function getConnection() {
-  const client = new Client(
-    {
-      host: 'localhost',
-      port: 5432,
-      user: 'mauricio',
-      password: 'admin123',
-      database: 'poke_store',
-    },
-  );
-  await client.connect();
-  return client;
-}
+// async function getConnection() {
+//   const client = new Client(
+//     {
+//       host: 'localhost',
+//       port: 5432,
+//       user: 'mauricio',
+//       password: 'admin123',
+//       database: 'poke_store',
+//     },
+//   );
+//   await client.connect();
+//   return client;
+// }
 
-module.exports = getConnection;
+// module.exports = getConnection;
